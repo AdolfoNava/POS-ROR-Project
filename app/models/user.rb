2149d3572b2b9e9982ad47d3000103ee0a52ddff
AdolfoNava <adolfonava1990@gmail.com>
2024-07-22 13:35:58 -0500

    Generate Devise User Table and Views

---
+++ app/models/user.rb
@@ -0,0 +1,27 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                     :integer          not null, primary key
+#  MODEL                  :string
+#  admin                  :boolean
+#  email                  :string           default(""), not null
+#  encrypted_password     :string           default(""), not null
+#  name                   :string
+#  remember_created_at    :datetime
+#  reset_password_sent_at :datetime
+#  reset_password_token   :string
+#
+# Indexes
+#
+#  index_users_on_email                 (email) UNIQUE
+#  index_users_on_reset_password_token  (reset_password_token) UNIQUE
+#
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  has_many  :orders, class_name: "Order", foreign_key: "employee_id", dependent: :nullify
+end
