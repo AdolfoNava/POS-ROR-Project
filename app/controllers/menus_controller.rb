3647c12769999aa40d632b9a5f773bc97a476f6d
AdolfoNava <adolfonava1990@gmail.com>
2024-07-23 16:22:41 -0500

    Database menu to User path and displaying collection

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -2,5 +2,7 @@ class MenusController < ApplicationController
   def main
     render "menus/main"
   end
-  
+  def database
+    render "menus/database"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/index' => "users#index", as: :list_users
   root to: "menus#main"
   get "customers/simple" => "customers#search_in_new_order", as: :find_customer
   get "orders/new" => "orders#new", as: :new_order
-  get "orders/new/:id" => "orders#new" as: :order
+  get "customers/orders/new/:id" => "orders#new", as: :order
+  get "database" => "menus#database"
   resources :options
   resources :categories
   devise_for :users
