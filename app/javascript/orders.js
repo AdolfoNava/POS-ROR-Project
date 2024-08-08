fdaffc5d9c57461ab4d1b80551fabe501f37491c
AdolfoNava <adolfonava1990@gmail.com>
2024-08-08 15:46:29 -0500

    Refactored item order form and js logic adding Bootstrap

--- app/javascript/orders.js
+++ app/javascript/orders.js
@@ -4,38 +4,48 @@ function addToOrder(option) {
   console.log($(elements[0]).text());
   console.log($(elements[1]).text());
 
-  let div = $('<div>');
-  let quantityinput = $('<input>')
+  let div = $('<div>').addClass('row mb-2');
+  let quantityDiv = $('<div>').addClass('col-md-2')
+  let quantityInput = $('<input>')
     .attr('type', 'number')
     .attr('name', `order[items_attributes][${count}][quantity]`)
     .attr('id', `order_items_attributes_${count}_quantity`)
     .attr('value', 1)
-    .addClass("numeric integer required");
+    .addClass("numeric integer required form-control");
 
-  let priceinput = $('<input>')
+  let priceDiv = $('<div>').addClass("col-md-2");
+  let priceInput = $('<input>')
     .attr('type', 'number')
     .attr('name', `order[items_attributes][${count}][price]`)
     .attr('id', `order_items_attributes_${count}_price`)
-    .attr('value', $(elements[1]).text());
+    .attr('value', $(elements[1]).text()).addClass('form-control text-end');
 
   let optioninput = $('<input>')
     .attr('type', 'hidden')
     .attr('name', `order[items_attributes][${count}][option_id]`)
     .attr('id', `order_items_attributes_${count}_option_id`)
     .attr('value', option);
-
-  let nameinput = $('<input>')
+  let nameDiv = $('<div>').addClass('col-md-8')
+  let nameInput = $('<input>')
     .attr('type', 'text')
     .attr('name', `order[items_attributes][${count}][name]`)
     .attr('id', `order_items_attributes_${count}_name`)
     .attr('value', $(elements[0]).text())
-    .addClass("string required");
+    .addClass("string required form-control");
 
-  div.append(quantityinput);
-  div.append(nameinput);
+  let colorPicker = $('<input>');
+
+  let colorConfirmer = $('<input>');
+  quantityDiv.append(quantityInput);
+  div.append(quantityDiv);
+  
+  nameDiv.append(nameInput);
+  div.append(nameDiv);
   div.append(optioninput);
-  div.append(priceinput);
+  priceDiv.append(priceInput)
+  div.append(priceDiv);
+
   $("#items-selected").append(div);
-  console.log(count);
+
   count++;
 }
