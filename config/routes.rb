e1037d9dea9dcf0c8b58b51662225008b23df813
AdolfoNava <adolfonava1990@gmail.com>
2024-08-14 11:18:37 -0500

    New User Registration removed from the routes and other helpful navigations

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   authenticate :user, ->(user) { user.admin? } do
     mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   end
-  devise_for :users
+  devise_for :users, :skip => [:registrations]
   unauthenticated do
     root "menus#landing", as: :landing
   end
