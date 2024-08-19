13e27e01f5cecbf664428d61a392399a543053ca
AdolfoNava <adolfonava1990@gmail.com>
2024-08-19 10:48:24 -0500

    rubocop formatted schema and auto_annotate_models

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,75 +12,74 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_10_130303) do
-  create_table "categories", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+ActiveRecord::Schema[7.1].define(version: 20_240_810_130_303) do # rubocop:disable Metrics/BlockLength
+  create_table 'categories', force: :cascade do |t|
+    t.string 'name'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "customers", force: :cascade do |t|
-    t.string "phone_number"
-    t.string "email"
-    t.string "last_name"
-    t.string "first_name"
-    t.string "address"
-    t.integer "orders_count"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'customers', force: :cascade do |t|
+    t.string 'phone_number'
+    t.string 'email'
+    t.string 'last_name'
+    t.string 'first_name'
+    t.string 'address'
+    t.integer 'orders_count'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "items", force: :cascade do |t|
-    t.integer "quantity"
-    t.string "name"
-    t.float "price"
-    t.integer "order_id"
-    t.integer "option_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'items', force: :cascade do |t|
+    t.integer 'quantity'
+    t.string 'name'
+    t.float 'price'
+    t.integer 'order_id'
+    t.integer 'option_id'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "options", force: :cascade do |t|
-    t.float "base_price"
-    t.string "name"
-    t.integer "category_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'options', force: :cascade do |t|
+    t.float 'base_price'
+    t.string 'name'
+    t.integer 'category_id'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "orders", force: :cascade do |t|
-    t.float "price"
-    t.date "due_date"
-    t.integer "customer_id"
-    t.integer "employee_id"
-    t.string "payment_method"
-    t.string "status"
-    t.float "pre_paid"
-    t.integer "items_count"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'orders', force: :cascade do |t|
+    t.float 'price'
+    t.date 'due_date'
+    t.integer 'customer_id'
+    t.integer 'employee_id'
+    t.string 'payment_method'
+    t.string 'status'
+    t.float 'pre_paid'
+    t.integer 'items_count'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.string "name"
-    t.boolean "admin"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  create_table 'users', force: :cascade do |t|
+    t.string 'email', default: '', null: false
+    t.string 'encrypted_password', default: '', null: false
+    t.string 'reset_password_token'
+    t.datetime 'reset_password_sent_at'
+    t.datetime 'remember_created_at'
+    t.string 'name'
+    t.boolean 'admin'
+    t.index ['email'], name: 'index_users_on_email', unique: true
+    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
   end
 
-  create_table "versions", force: :cascade do |t|
-    t.string "item_type", null: false
-    t.bigint "item_id", null: false
-    t.string "event", null: false
-    t.string "whodunnit"
-    t.text "object", limit: 1073741823
-    t.datetime "created_at"
-    t.index ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id"
+  create_table 'versions', force: :cascade do |t|
+    t.string 'item_type', null: false
+    t.bigint 'item_id', null: false
+    t.string 'event', null: false
+    t.string 'whodunnit'
+    t.text 'object', limit: 1_073_741_823
+    t.datetime 'created_at'
+    t.index %w[item_type item_id], name: 'index_versions_on_item_type_and_item_id'
   end
-
 end
